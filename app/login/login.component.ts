fdde4bb9af64215f745f3bdc8cb990a7c53a039e
unknown <paxtonantony@gmail.com>
2023-01-30 21:46:57 +0530

    bank

---
+++ app/login/login.component.ts
@@ -0,0 +1,44 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+  acno=""
+  psw=""
+
+data="welcome to SBI BANK"
+  input="account number"
+  userDetails:any={
+    1000:{acno:1000,username:"anu",password:"abc123",balance:0},
+    1001:{acno:1001,username:"amal",password:"abc123",balance:0},
+    1002:{acno:1002,username:"arun",password:"abc1234",balance:0},
+    1003:{acno:1000,username:"akil",password:"abc1234",balance:0}
+
+  }
+  constructor() { }
+  ngOnInit(): void{
+
+  }
+
+  login(){
+    alert('login clicked')
+  }
+
+  acnoChange(event:any){
+    this.acno=event.target.value
+    console.log(this.acno);
+    
+  }
+  pwrdChange(event:any){
+    this.psw=event.target.value
+    console.log(this.psw);
+    
+  }
+  
+
+
+
+}
