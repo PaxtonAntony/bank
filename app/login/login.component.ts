4e1be0eefc72ed0e446cf06dba5d6d4fefd03589
unknown <paxtonantony@gmail.com>
2023-01-31 10:08:59 +0530

    bank

--- app/login/login.component.ts
+++ app/login/login.component.ts
@@ -6,8 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-  acno=""
-  psw=""
+  // acno=""
+  // psw=""
 
 data="welcome to SBI BANK"
   input="account number"
@@ -22,23 +22,60 @@ data="welcome to SBI BANK"
   ngOnInit(): void{
 
   }
+// }
 
-  login(){
-    alert('login clicked')
-  }
+  // login(){
+  //   var acnum=this.acno
+  //   var psw=this.psw
+  //   var userDetails=this.userDetails
+  //   if(acnum in userDetails){
+  //     if(psw==userDetails[acnum]["password"]){
+  //       alert("login successfull")
+  //     }
+  //         else{
+  //           alert("password is incorrect")
+  //         }
+        
+  //     }
 
-  acnoChange(event:any){
-    this.acno=event.target.value
-    console.log(this.acno);
     
-  }
-  pwrdChange(event:any){
-    this.psw=event.target.value
-    console.log(this.psw);
+  //   else{
+  //     alert("acnum is invalid")
+  // }
+
+  // }
+  // acnoChange(event:any){
+  //   this.acno=event.target.value
+  //  // console.log(this.acno);
     
-  }
+  // }
+  // pwrdChange(event:any){
+  //   this.psw=event.target.value
+  //   //console.log(this.psw);
+    
+  // }
+
   
 
+login(a:any,b:any){
+  var acnum=a.value
+  var psw=b.value
+  var userDetails=this.userDetails
+  if(acnum in userDetails){
+    if(psw==userDetails[acnum]["password"]){
+      alert("login successfull")
+    }
+        else{
+          alert("password is incorrect")
+        }
+      
+    }
+
+  
+  else{
+    alert("acnum is invalid")
+ }
+}
 
 
 }
