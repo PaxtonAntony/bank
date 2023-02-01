834c5b9263df9c6407f82291119335e2c39f30a0
unknown <paxtonantony@gmail.com>
2023-02-01 10:53:16 +0530

    bank

--- app/login/login.component.ts
+++ app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { DataService } from '../Services/data.service';
 
 @Component({
   selector: 'app-login',
@@ -6,44 +8,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-  // acno=""
-  // psw=""
+  acno=""
+  psw=""
 
 data="welcome to SBI BANK"
   input="account number"
-  userDetails:any={
-    1000:{acno:1000,username:"anu",password:"abc123",balance:0},
-    1001:{acno:1001,username:"amal",password:"abc123",balance:0},
-    1002:{acno:1002,username:"arun",password:"abc1234",balance:0},
-    1003:{acno:1000,username:"akil",password:"abc1234",balance:0}
-
-  }
-  constructor() { }
+  constructor(private router:Router,private ds:DataService) { }
   ngOnInit(): void{
 
   }
-// }
 
-  // login(){
-  //   var acnum=this.acno
-  //   var psw=this.psw
-  //   var userDetails=this.userDetails
-  //   if(acnum in userDetails){
-  //     if(psw==userDetails[acnum]["password"]){
-  //       alert("login successfull")
-  //     }
-  //         else{
-  //           alert("password is incorrect")
-  //         }
+  login(){
+    var acnum=this.acno
+    var psw=this.psw
+    var userDetails=this.ds.userDetails
+    if(acnum in userDetails){
+      if(psw==userDetails[acnum]["password"]){
+        alert("login successfull")
+        this.router.navigateByUrl('dashboard')
+      }
+          else{
+            alert("password is incorrect")
+          }
         
-  //     }
+      }
 
     
-  //   else{
-  //     alert("acnum is invalid")
-  // }
+    else{
+      alert("acnum is invalid")
+  }
 
-  // }
+  }
   // acnoChange(event:any){
   //   this.acno=event.target.value
   //  // console.log(this.acno);
@@ -57,25 +52,25 @@ data="welcome to SBI BANK"
 
   
 
-login(a:any,b:any){
-  var acnum=a.value
-  var psw=b.value
-  var userDetails=this.userDetails
-  if(acnum in userDetails){
-    if(psw==userDetails[acnum]["password"]){
-      alert("login successfull")
-    }
-        else{
-          alert("password is incorrect")
-        }
+// login(a:any,b:any){
+//   var acnum=a.value
+//   var psw=b.value
+//   var userDetails=this.userDetails
+//   if(acnum in userDetails){
+//     if(psw==userDetails[acnum]["password"]){
+//       alert("login successfull")
+//     }
+//         else{
+//           alert("password is incorrect")
+//         }
       
-    }
+//     }
 
   
-  else{
-    alert("acnum is invalid")
+//   else{
+//     alert("acnum is invalid")
+//  }
+// }
+
+
  }
-}
-
-
-}
